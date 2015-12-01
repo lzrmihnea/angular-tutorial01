0f37a9e7eecf9d7408e946e5b73b169c183388c4
lzrmihnea <lzr_mihnea@yahoo.com>
2015-12-01 02:11:25 +0200

    [Codeschool, Level-02] Cleaned up code

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 (function () {
     var app = angular.module('gemStore', []);
 
-    app.controller('StoreController', function () {
-        this.products = gems;
-    });
-
     var gems = [
         {
             name: 'Dodecahedron',
@@ -41,14 +37,19 @@
             ]
         }
     ]
-})();
 
-app.controller("PanelController", function(){
-    this.tab = 1;
-    this.selectTab = function(setTab) {
-        this.tab = setTab;
-    };
-    this.isSelected = function(checkTab) {
-        return this.tab === checkTab;
-    }
-});
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    app.controller("PanelController", function(){
+        this.tab = 1;
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab) {
+            return (this.tab === checkTab);
+        }
+    });
+
+})();
