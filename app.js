ed9e448ea8e2b256ec85c845132035b72fe60646
lzrmihnea <lzr_mihnea@yahoo.com>
2015-12-01 14:42:02 +0200

    Invalid form borders highlighted

--- app.js
+++ app.js
@@ -65,6 +65,7 @@
     });
 
     app.controller("ReviewController", function() {
+        this.review = {};
 
         this.addReview = function(product) {
             product.reviews.push(this.review);
