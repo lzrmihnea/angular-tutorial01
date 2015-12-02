a093670ef41b54518a36773989285d0e9b0f5d6e
lzrmihnea <lzr_mihnea@yahoo.com>
2015-12-03 00:23:48 +0200

    Added image-gallery directive

--- app.js
+++ app.js
@@ -72,6 +72,21 @@
        };
     });
 
+    app.directive("productGallery", function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'views/product-gallery.html',
+            controller:function(){
+                this.current = 0;
+                this.setCurrent = function(imageNumber){
+                    this.current = imageNumber || 0;
+                };
+            },
+            controllerAs:'gallery'
+        };
+
+    });
+
     app.directive('productPanels', function(){
         return {
             restrict: 'E',   // Type of Directive (E for Element)
